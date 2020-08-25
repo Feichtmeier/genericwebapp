205dc890433b550c9ba5fc100e704e056397f11d
Feichtmeier <frederik.feichtmeier@gmail.com>
2020-08-25 23:50:27 +0200

    remove wildcard

--- genericwebapp/src/main/java/org/feichtmeier/genericwebapp/security/SecurityConfiguration.java
+++ genericwebapp/src/main/java/org/feichtmeier/genericwebapp/security/SecurityConfiguration.java
@@ -101,8 +101,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/VAADIN/**",
 
-                "/**",
-
                 "/api/**",
 
                 "/favicon.ico",
